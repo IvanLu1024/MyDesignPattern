2e4edb4320bb9b5d935da9760c160d45931bdfc9
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2018-08-22 21:27:52 +0800

    添加注释

--- src/main/java/behavioural/strategy/QuackBehavior.java
+++ src/main/java/behavioural/strategy/QuackBehavior.java
@@ -1,6 +1,10 @@
 package behavioural.strategy;
 
-
+/**
+ * 相当于上下文对象，这个对象负责持有算法，但是不负责
+ * 决定具体选用哪个算法，把选择算法的功能交给了用户
+ * 这样一来，具体的算法和直接使用算法的用户就不是耦合的，是分离的。
+ */
 public interface QuackBehavior {
 
     void quck();
